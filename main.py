b5de1029a22855ce876ded2374f27c0fea351c9d
kerry1715 <conorhealy567@gmail.com>
2020-03-09 17:40:20 +0000

    adding repl.it support

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import mobTables
 
-mobTables.grassField()
+print("You are in the town of startsville,")
+
+running = True
+inStartsville = True
+
+while running:
+    if inStartsville:
+        choice = input("you can input TALK to talk to the villagers, or input TRAIN to head to the nearby grass field: ")
+        if choice.lower() == "train":
+            mobTables.grassField()
+        elif choice.lower() == "talk":
+            print("This feature is in development")
